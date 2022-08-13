3f3d25501a65967ba2cf31abd8930685349b5dd3
Lucho Romero <go.romerolae@gmail.com>
2022-08-12 22:33:18 -0500

    added all features

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import CardResidents from './components/CardResidents';
 import LocationInfo from './components/LocationInfo';
-import useFetch from './hooks/useFetch';
 
 function App() {
-	const randomLocation = Math.floor(Math.random() * (126 - 1) + 1);
+	const [searchInput, setSearchInput] = useState('');
+	const [location, setLocation] = useState();
 
-	const URL = `https://rickandmortyapi.com/api/location/${randomLocation}`;
+	useEffect(() => {
+		let numberLocation;
+		if (searchInput === '') {
+			numberLocation = Math.floor(Math.random() * (126 - 1) + 1);
+		} else {
+			numberLocation = searchInput;
+		}
 
-	const location = useFetch(URL);
+		const URL = `https://rickandmortyapi.com/api/location/${numberLocation}`;
+		axios
+			.get(URL)
+			.then((res) => setLocation(res.data))
+			.catch((err) => console.log(err));
+	}, [searchInput]);
+
+	//Busqueda por input
+
+	const handleSumit = (e) => {
+		e.preventDefault();
+		setSearchInput(e.target.search.value);
+	};
 
 	return (
 		<div className="App">
 			<h1>Rick and Morty</h1>
+			<form onSumit={handleSumit}>
+				<input id="search" type="text" />
+				<button>Search</button>
+			</form>
 			<LocationInfo location={location} />
 			<div>
 				{location?.residents.map((url) => (
--- src/components/CardResidents.jsx
+++ src/components/CardResidents.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 const CardResidents = ({ url }) => {
-	console.log(url);
+	const resident = useFetch(url);
+
+	return (
+		<article>
+			<header>
+				<img src={resident?.image} alt={`image of ${resident?.name}`} />
+				<div>
+					<div className="circle"></div>
+					<span>{resident?.status}</span>
+				</div>
+			</header>
+			<div>
+				<h3>{resident?.name}</h3>
+				<ul>
+					<li>
+						<span>Species:</span>
+						{resident?.species}
+					</li>
+					<li>
+						<span>Origin: </span>
+						{resident?.origin.name}
+					</li>
+					<li>
+						<span>Episodes where appear: </span>
+						{resident?.episode.length}
+					</li>
+				</ul>
+			</div>
+		</article>
+	);
 };
 
 export default CardResidents;
